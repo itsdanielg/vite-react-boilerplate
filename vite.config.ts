beaa774798d091e8575d88f5e17ef913047a9644
itsdanielg <danielg_97@yahoo.com>
2023-09-29 13:06:23 -0400

    Added tsconfig paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: "./",
+  plugins: [react(), tsConfigPaths()],
   server: {
     port: 3000,
     host: true
