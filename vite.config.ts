0a32927983a13d7b77b8981919492d3f62ce8542
itsdanielg <danielg_97@yahoo.com>
2025-12-01 23:08:39 -0500

    Tailwind update

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,14 @@
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
-  plugins: [react(), tsConfigPaths()],
-  server: {
-    port: 3000,
-    host: true
-  },
+  plugins: [react(), tsConfigPaths(), tailwindcss()],
+  server: { port: 3000, strictPort: true, host: true },
+  preview: { port: 8080, strictPort: true, host: true },
   test: {
     globals: true,
     environment: "jsdom",
