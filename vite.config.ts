1504042d775f2e4cd3938682341f03ed5d8c8d85
itsdanielg <danielg_97@yahoo.com>
2026-01-10 22:36:54 -0500

    Tailwind v4 migration

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import tsConfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
   plugins: [react(), tsConfigPaths(), tailwindcss()],
